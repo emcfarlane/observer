119dff3574b936b47ccc2255e34104ea48127db8
Edward McFarlane <edward.mcfarlane@outlook.com>
2019-08-07 21:53:52 +0000

    io pipe sync.Cond

--- pipe.go
+++ pipe.go
@@ -1,8 +1,8 @@
 package observer
 
 import (
-	"sync"
 	"io"
+	"sync"
 )
 
 type pipeState int
@@ -14,13 +14,14 @@ const (
 )
 
 type pipe struct {
-	mu sync.Mutex
+	mu    sync.Mutex
+	cond  sync.Cond
 	state pipeState
 
 	rwMu sync.Mutex
-	b []byte
-	n int
-	err error
+	b    []byte
+	n    int
+	err  error
 }
 
 func (p *pipe) copy(b []byte, state pipeState) (n int, err error) {
@@ -30,31 +31,20 @@ func (p *pipe) copy(b []byte, state pipeState) (n int, err error) {
 		return 0, p.err
 	}
 
-	for p.state != -state {
-		if p.err != nil {
-			p.mu.Unlock()
-			return 0, p.err
-		}
+	for p.state == state {
+		p.cond.Wait() // Queue of same types.
+	}
 
-		next := p.state == pipeOpen
-		if next {
-			p.rwMu.Lock() // Enqueue
-			p.state = state
-			p.b = b
-		}
+	if p.state == pipeOpen {
+		p.b, p.state = b, state
+		p.rwMu.Lock()
 		p.mu.Unlock()
 
-		// TODO: sync.Cond.Signal?
-		p.rwMu.Lock() // Sync on rwMU
-		if !next {
-			p.rwMu.Unlock()
-			p.mu.Lock()
-			continue
-		}
-
+		p.rwMu.Lock() // Sync.
 		n, err = p.n, p.err
 		p.state = pipeOpen
 		p.rwMu.Unlock()
+		p.cond.Signal()
 		p.mu.Unlock()
 		return
 	}
@@ -67,14 +57,13 @@ func (p *pipe) copy(b []byte, state pipeState) (n int, err error) {
 	n = copy(dst, src)
 	p.n = n
 	p.rwMu.Unlock()
-	return 
+	return
 }
 
 func (p *pipe) close(err error) error {
 	p.mu.Lock()
 	if p.err == nil {
-		p.n = 0
-		p.err = err
+		p.n, p.err = 0, err
 	}
 	if p.state != pipeOpen {
 		p.rwMu.Unlock()
@@ -84,24 +73,31 @@ func (p *pipe) close(err error) error {
 	return p.err
 }
 
-func (p *pipe) Read(b []byte) (int, error) { return p.copy(b, pipeRead) }
-func (p *pipe) Write(b []byte) (int, error) { return p.copy(b, pipeWrite) }
-
 type PipeReader struct {
 	p *pipe
 }
 
-func (pr *PipeReader) Read(p []byte) (int, error) { return pr.p.Read(p) }
-func (pr *PipeReader) Close() error { return pr.p.close(io.ErrClosedPipe) }
+func (pr *PipeReader) Read(b []byte) (int, error) {
+	return pr.p.copy(b, pipeRead)
+}
+func (pr *PipeReader) Close() error {
+	return pr.p.close(io.ErrClosedPipe)
+}
 
 type PipeWriter struct {
 	p *pipe
 }
 
-func (pw *PipeWriter) Write(p []byte) (int, error) { return pw.p.Write(p) }
-func (pw *PipeWriter) Close() error { return pw.p.close(io.EOF) }
+func (pw *PipeWriter) Write(b []byte) (int, error) {
+	return pw.p.copy(b, pipeWrite)
+}
+func (pw *PipeWriter) Close() error {
+	return pw.p.close(io.EOF)
+}
 
+// Pipe is an io.Pipe implementation.
 func Pipe() (*PipeReader, *PipeWriter) {
 	p := &pipe{}
+	p.cond.L = &p.mu
 	return &PipeReader{p}, &PipeWriter{p}
 }
--- pipe_test.go
+++ pipe_test.go
@@ -1,17 +1,17 @@
 package observer
 
 import (
-	"testing"
-	"io/ioutil"
 	"io"
+	"io/ioutil"
+	"testing"
 )
 
 func TestPipe(t *testing.T) {
 	r, w := Pipe()
 
 	type result struct {
-		b []byte
-		n int
+		b   []byte
+		n   int
 		err error
 	}
 	rs := make(chan result)
@@ -19,29 +19,31 @@ func TestPipe(t *testing.T) {
 	go func() {
 		n, err := w.Write([]byte("hello"))
 		if err != nil {
-			rs<-result{n: n, err: err}	
+			rs <- result{n: n, err: err}
 		}
 		w.Close()
 	}()
 
 	go func() {
 		b, err := ioutil.ReadAll(r)
-		rs<-result{b: b, n: len(b), err: err}
+		rs <- result{b: b, n: len(b), err: err}
 	}()
 
 	x := <-rs
 	if x.err != nil {
 		t.Error(x)
 	}
-	//t.Fatalf("%s %v %v", x.b, x.n, x.err)
+	if string(x.b) != "hello" {
+		t.Fatalf("%s %v %v", x.b, x.n, x.err)
+	}
 }
 
 func BenchmarkPipe(b *testing.B) {
 	r, w := Pipe()
 
 	type result struct {
-		b []byte
-		n int
+		b   []byte
+		n   int
 		err error
 	}
 	rs := make(chan result)
@@ -51,9 +53,9 @@ func BenchmarkPipe(b *testing.B) {
 		rs <- result{b: b, n: len(b), err: err}
 	}()
 
-        for n := 0; n < b.N; n++ {
+	for n := 0; n < b.N; n++ {
 		w.Write([]byte("go"))
-        }
+	}
 	w.Close()
 
 	x := <-rs
@@ -62,13 +64,12 @@ func BenchmarkPipe(b *testing.B) {
 	}
 }
 
-
 func BenchmarkPipeIO(b *testing.B) {
 	r, w := io.Pipe()
 
 	type result struct {
-		b []byte
-		n int
+		b   []byte
+		n   int
 		err error
 	}
 	rs := make(chan result)
@@ -78,9 +79,9 @@ func BenchmarkPipeIO(b *testing.B) {
 		rs <- result{b: b, n: len(b), err: err}
 	}()
 
-        for n := 0; n < b.N; n++ {
+	for n := 0; n < b.N; n++ {
 		w.Write([]byte("go"))
-        }
+	}
 	w.Close()
 
 	x := <-rs
